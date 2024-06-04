22b3a5f861afeeb20ada5233ab44fee3b115bd9e
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-04 14:50:27 +0530

    redirected to view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function index()
     {
+        // dd(Auth::user());
         return view('Admin.Home.Index');
     }
 }
