6466425b3985eaac90dff4ffc3f2ff3c94e052d2
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-04 14:09:11 +0530

    chnaged path

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,8 +24,8 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('admin-filament')
+            ->path('admin-filament')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
