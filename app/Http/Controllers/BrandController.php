ed92c466a63ca83761bbbfc7384b4c243f3e0d3c
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-07 15:59:35 +0530

    create method to database

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,16 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BrandController extends Controller
 {
     public function Index()
     {
-        return view('Brands.Index');
+        $brands = Brand::all();
+        // $brands = DB::table('brands')->get();
+        // $brands = DB::select('Select * from brands');
+
+        // dd($brands);
+        return view('Brands.Index',compact('brands'));
+        // return view('Brands.Index');
     }
     public function CreateForm()
     {
         return view('Brands.Create');
+    }
+    public function Save(Request $request)
+    {
+        // dd($request);
+        Brand::create([
+            'name'=>$request->name,
+            'seller'=>$request->seller,
+            'origin'=>$request->origin,
+            'location'=>$request->location,
+            'rating'=>$request->rating,
+        ]);
+
+        return redirect('/admin/brands');
+
     }
 }
