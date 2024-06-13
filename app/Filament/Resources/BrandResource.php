328532448920f227ac2e41142a095a941ae5c48f
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-13 15:11:31 +0530

    brand resources

---
+++ app/Filament/Resources/BrandResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BrandResource\Pages;
+use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Models\Brand;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BrandResource extends Resource
+{
+    protected static ?string $model = Brand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+            TextInput::make('location'),
+            TextInput::make('origin'),
+            TextInput::make('seller'),
+            TextInput::make('rating'),
+            FileUpload::make('logo'),
+                ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('name'),
+                TextColumn::make('seller'),
+                TextColumn::make('location'),
+                TextColumn::make('origin'),
+                ImageColumn::make('logo')
+                // TextColumn::make('rating'),
+                ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBrands::route('/'),
+            'create' => Pages\CreateBrand::route('/create'),
+            'edit' => Pages\EditBrand::route('/{record}/edit'),
+        ];
+    }
+}
