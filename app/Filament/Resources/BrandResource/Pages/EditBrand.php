15a36241cb0f5af06b1fa919c4f5b79ce57a1dd7
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-13 15:11:52 +0530

    brand resources

---
+++ app/Filament/Resources/BrandResource/Pages/EditBrand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\BrandResource\Pages;
+
+use App\Filament\Resources\BrandResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBrand extends EditRecord
+{
+    protected static string $resource = BrandResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
