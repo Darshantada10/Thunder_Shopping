f1f5adeaffa614215860a6f09a85d81709552e10
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-22 15:11:56 +0530

    commented fields

--- database/migrations/2024_06_13_093533_add_brands_table.php
+++ database/migrations/2024_06_13_093533_add_brands_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('brands', function (Blueprint $table) {
-            $table->string('logo')->nullable();
+            // $table->string('logo')->nullable();
         });
     }
 
