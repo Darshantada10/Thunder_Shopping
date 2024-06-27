3d0f3ee9701659fa957dc2e427e378fade957d58
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-27 16:12:30 +0530

    new table

--- database/migrations/2024_06_22_090001_create_products_table.php
+++ database/migrations/2024_06_22_090001_create_products_table.php
@@ -13,7 +13,21 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('brand_id');
+            // $table->unsignedBigInteger('b_id');
+            // $table->unsignedBigInteger('bid');
+            // $table->unsignedBigInteger('brand_ka_refference');
+            // $table->unsignedBigInteger('kuch_bhi');
+
             $table->json('product_data');
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            // $table->foreign('b_id')->references('id')->on('brands')->onDelete('cascade');
+            // $table->foreign('bid')->references('id')->on('brands')->onDelete('cascade');
+            // $table->foreign('brand_ka_referrence')->references('id')->on('brands')->onDelete('cascade');
+            // $table->foreign('kuch_bhi')->references('id')->on('brands')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
