cee6fb8d57800d5da39db4de0bb09267da9ed1f5
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-27 16:12:17 +0530

    relationship with brand

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $fillable = ['product_data'];
+    protected $fillable = ['product_data','brand_id'];
 
     protected $casts = ['product_data'=>'json'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class,'brand_id');
+    }
 }
