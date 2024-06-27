e7614d164bb516eb4b21aa1c4c9f7b2d83881ef9
Darshantada10 <darshantadaofficial@gmail.com>
2024-06-27 16:11:34 +0530

    testing function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -13,4 +14,19 @@ class AdminController extends Controller
         // dd(Auth::user());
         return view('Admin.Home.Index');
     }
+    public function Testing()
+    {
+        // dd("inside controller");
+        // $products = Product::whereHas('brands', function($query)
+        // {
+        //     $query->where('name');
+        // })->get();
+
+        $products =  Product::find(5);
+        $products= $products->brand;
+
+        // $data = $products->brand();
+        dd($products);
+    }
+
 }
