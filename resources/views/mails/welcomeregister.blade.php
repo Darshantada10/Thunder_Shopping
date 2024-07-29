425ca76722e69d138d9463bd772edd4b2a7b6768
Darshantada10 <darshantadaofficial@gmail.com>
2024-07-29 14:53:23 +0530

    displayed name

--- resources/views/mails/welcomeregister.blade.php
+++ resources/views/mails/welcomeregister.blade.php
@@ -87,6 +87,7 @@
 }
 </style>
   </head>
+    <h1>Welcome Customer {{$user}} Happy Shopping/h1>
   <body style="font-family: Helvetica, sans-serif; -webkit-font-smoothing: antialiased; font-size: 16px; line-height: 1.3; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; background-color: #f4f5f6; margin: 0; padding: 0;">
     <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f4f5f6; width: 100%;" width="100%" bgcolor="#f4f5f6">
       <tr>
