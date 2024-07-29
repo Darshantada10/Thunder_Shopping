e0772e01b338af0646ac50894753879f68f457e1
Darshantada10 <darshantadaofficial@gmail.com>
2024-07-29 14:53:03 +0530

    grouping and prefix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,11 +29,41 @@ Route::get('/testing',[AdminController::class,"Testing"]);
 
 Route::get('/admin',[AdminController::class,'index']);
 
-Route::get('/admin/brands',[BrandController::class,'Index']);
-Route::get('/api/all-brands',[BrandController::class,'APIBrands']);
-Route::get('/admin/brand/create',[BrandController::class,'CreateForm']);
-Route::post('/admin/brand/create',[BrandController::class,'Save']);
 
-Route::get('/admin/brand/update/{id}',[BrandController::class,'UpdateForm']);
-Route::post('/admin/brand/update/{id}',[BrandController::class,'UpdateData']);
-Route::get('/admin/brand/{id}/delete',[BrandController::class,'DeleteBrand']);
+
+Route::prefix('/admin')->group(function(){
+
+    
+    Route::get('/brands',[BrandController::class,'Index']);
+    Route::get('/api/all-brands',[BrandController::class,'APIBrands']);
+    Route::get('/brand/create',[BrandController::class,'CreateForm']);
+    Route::post('/brand/create',[BrandController::class,'Save']);
+    
+    Route::get('/brand/update/{id}',[BrandController::class,'UpdateForm']);
+    Route::post('/brand/update/{id}',[BrandController::class,'UpdateData']);
+    Route::get('/brand/{id}/delete',[BrandController::class,'DeleteBrand']);
+    
+    // put patch delete any 
+    
+    // Route::get('/admin/products',[ProductController::class,'index']);
+
+    Route::controller(ProductController::class)->group(function(){
+    
+    Route::get('/products','index');
+
+    Route::get('/product/create','createform');
+    Route::post('/product/create','savedata');
+    
+    Route::get('/product/update/{id}','updateform');
+    Route::post('/product/update/{id}','saveupdatedata');
+    
+    Route::get('/product/delete/{id}','deletedata');
+});
+
+
+
+
+
+
+
+});
