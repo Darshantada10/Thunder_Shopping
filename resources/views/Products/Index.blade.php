d9f9c6c32c062ea93cbdc277bad6b24dff133d59
Darshantada10 <darshantadaofficial@gmail.com>
2024-07-30 14:58:06 +0530

    index page for product

---
+++ resources/views/Products/Index.blade.php
@@ -0,0 +1,65 @@
+@extends('Layouts.AdminApp')
+
+
+@section('content')
+
+{{-- {{dd($brands)}} --}}
+
+@if (session('success'))
+      <div class="alert alert-success">{{session('success')}}</div>
+@endif
+@if (session('danger'))
+      <div class="alert alert-danger">{{session('danger')}}</div>
+@endif
+@if (session('primary'))
+      <div class="alert alert-primary">{{session('primary')}}</div>
+@endif
+
+
+<div class="card">
+    <h5 class="card-header">
+        All Products
+        <a href="{{url('/admin/product/create')}}" class="btn rounded-pill btn-primary float-end">
+            <i class="bx bx-list-plus"></i> Add Product</a>
+
+
+
+    </h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <thead>
+          <tr class="text-nowrap">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Seller</th>
+            <th>Location</th>
+            <th>Brand Origin</th>
+            <th>Rating</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {{-- @foreach ($brands as $brand)
+          <tr>
+            
+            <th scope="row">{{$brand->id}}</th>
+            <td>{{$brand->name}}</td>
+            <td>{{$brand->seller}}</td>
+            <td>{{$brand->location}}</td>
+            <td>{{$brand->origin}}</td>
+            <td>{{$brand->rating}}</td>
+            <td>
+                <a href="{{url('/admin/brand/update',['id'=>$brand->id])}}" class="btn btn-sm btn-primary text-white">Update</a>
+                <a href="/admin/brand/{{$brand->id}}/delete/" class="btn btn-sm btn-danger text-white">Delete</a>
+            </td>
+            
+          </tr>
+
+            @endforeach --}}
+            
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+@endsection
