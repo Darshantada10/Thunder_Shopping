626f3030988281014e13bf8764791650dfcc77ee
Darshantada10 <darshantadaofficial@gmail.com>
2024-07-30 14:59:23 +0530

    new methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('Products.Index');
+    }
+    public function createform()
+    {
+        $brands = Brand::all();
+        // dd($brands);
+        return view('Products.Create',compact('brands'));
+    }
+    public function savedata(Request $request)
+    {
+        
+        // dd($request);
+        // Brand::create([
+        //     'name'=>$request->name,
+        //     'seller'=>$request->seller,
+        //     'origin'=>$request->origin,
+        //     'location'=>$request->location,
+        //     'rating'=>$request->rating,
+        //     // 'logo'=>$upload_logo,
+        // ]);
+
+        return redirect('/admin/brands')->with('primary','Brand Created Successfully');
+    }
 }
