23c70b7f2156d9e6f387374dc10b92efdc83d73e
Darshantada10 <darshantadaofficial@gmail.com>
2024-07-30 14:59:58 +0530

    status message

--- resources/views/Home/Index.blade.php
+++ resources/views/Home/Index.blade.php
@@ -3,6 +3,25 @@
 @section('content')
 
 
+@if (session('danger'))
+
+<div class="alert alert-danger" role="alert">
+    {{session('danger')}}
+</div>
+
+    
+@endif
+
+@if (session('warning'))
+
+<div class="alert alert-warning" role="alert">
+    {{session('warning')}}
+</div>
+
+    
+@endif
+
+
 <hr>
 <hr>
 <br>
