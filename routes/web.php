e3b62de9381f094767e08a2d0c0d809535237ac5
Darshantada10 <darshantadaofficial@gmail.com>
2024-08-05 14:01:15 +0530

    new template

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BrandController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     // return view('layouts/App');
 //     return view('Layouts.App');
 // });
+
+
+Route::get('/clear-commands',function()
+{
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('optimize');
+    return "commands run successfully";
+});
+
 
 Route::get('/',[HomeController::class,'index']);
 Route::get('/home',[HomeController::class,'index']);
