4e0c3a923b0fc5c7bfe65d18b05200767a31d142
Darshantada10 <darshantadaofficial@gmail.com>
2024-08-12 15:24:34 +0530

    new model

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'image',
+        'meta_title',
+        'meta_keyword',
+        'meta_description',
+        'other_details',
+        'status'
+    ];
+
+    protected $casts = ['other_details'=>'json','status'=>'boolean'];
+
+
+}
