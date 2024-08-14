d4b2c2000a5a993c7938e04b381e6c50ea880619
Darshantada10 <darshantadaofficial@gmail.com>
2024-08-14 15:22:51 +0530

    brand table

---
+++ database/migrations/2024_08_14_081604_create_brands_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            
+            $table->string('name');
+            $table->string('slug');
+            $table->tinyInteger('status')->default(0)->comment('0 = visible , 1 = hidden');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
