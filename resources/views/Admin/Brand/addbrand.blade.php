3060b609dad20966efc86387a4628a58b8870c1e
Darshantada10 <darshantadaofficial@gmail.com>
2024-08-16 15:28:40 +0530

    updated modal

--- resources/views/Admin/Brand/addbrand.blade.php
+++ resources/views/Admin/Brand/addbrand.blade.php
@@ -78,7 +78,7 @@
 
 
             {{-- <form onsubmit="event.preventDefault(); branddata();" > --}}
-            <form id="BrandData" >
+            <form id="UpdateBrandData" >
                 <div class="modal-body">
 
                     <div class="mb-3">
@@ -93,7 +93,7 @@
                             <small class="text-danger">{{$message}}</small>
                         @enderror
                     </div>
-
+                    <input type="hidden" id="id" name="id" >
                     <div class="mb-3">
                         <label for="name">Brand Name</label>
                         <input type="text" name="name" id="name" class="form-control">
@@ -145,7 +145,7 @@
 
 
             {{-- <form onsubmit="event.preventDefault(); branddata();" > --}}
-            <form id="BrandData" >
+            <form id="DeleteBrandData" >
                 <div class="modal-body">
 
                  <h3>Are you sure you want to delete this brand?</h3>
@@ -156,7 +156,7 @@
                 
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-success">Save</button>
+                    <button type="button" id="confirmdelete" class="btn btn-danger">Delete</button>
                 </div>
                 
             </form>
@@ -172,6 +172,44 @@
     
     <script>
 
+function deletebrand(id)
+        {
+
+            document.getElementById('confirmdelete').onClick = function(){
+
+              
+              
+              
+            fetch(`/admin/api/brand/delete/${id}`,{
+                method: 'get',
+                // body: brandformdata,
+                headers: {'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')}
+            }).then(response=>response.json())
+            .then(data=> {
+
+                // console.log(data.success);
+
+                // document.getElementById('sucess') = data.success;
+                // $('#success').text(data.success).show();
+                
+                // $('#updatebrand').modal('hide')
+                // $('#updatebrand input').val('')
+                // $('#updatebrand input[type="checkbox"]').prop('checked',false)
+                // $('#updatebrand select').val('')
+
+                document.getElementById('success').innerText = data.success;
+                displayallbrands();
+              }).catch(error => console.error('Error : ',error));
+              
+            }
+          // var deletebrandmodal = document.getElementById('deletebrand');
+
+          // deletebrandmodal.addEventListener('show.bs.modal',function(event){
+            
+          // })
+
+        }
+        
     // function branddata()
     // {
     //     console.log("inside function");
@@ -218,6 +256,91 @@
             }).catch(error => console.error('Error : ',error));
 
         });
+
+        document.getElementById('UpdateBrandData').addEventListener('submit',function(event){
+            event.preventDefault();
+
+
+
+            var brandformdata = new FormData(this)
+            // console.log(brandformdata);
+            
+            var data = ""
+            brandformdata.forEach(function (value,key) 
+            {
+                data += `${key} + " : " + ${value} `;
+                // console.log(key , value);
+                    
+            });
+            var updateid = brandformdata.get('id')
+            // console.log(updateid);
+            
+
+            fetch(`/admin/api/brand/update/${updateid}`,{
+                method: 'post',
+                body: brandformdata,
+                headers: {'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')}
+            }).then(response=>response.json())
+            .then(data=> {
+
+                // console.log(data.success);
+
+                // document.getElementById('sucess') = data.success;
+                // $('#success').text(data.success).show();
+                
+                $('#updatebrand').modal('hide')
+                $('#updatebrand input').val('')
+                $('#updatebrand input[type="checkbox"]').prop('checked',false)
+                $('#updatebrand select').val('')
+
+                document.getElementById('success').innerText = data.success;
+                displayallbrands();
+            }).catch(error => console.error('Error : ',error));
+
+        });
+      
+      
+        // document.getElementById('DeleteBrandData').addEventListener('submit',function(event){
+        //     // event.preventDefault();
+
+
+
+        //     // var brandformdata = new FormData(this)
+        //     // console.log(brandformdata);
+            
+        //     // var data = ""
+        //     // brandformdata.forEach(function (value,key) 
+        //     // {
+        //         // data += `${key} + " : " + ${value} `;
+        //         // console.log(key , value);
+                    
+        //     // });
+        //     // var updateid = brandformdata.get('id')
+        //     // console.log(updateid);
+            
+
+        //     fetch(`/admin/api/brand/delete/${updateid}`,{
+        //         method: 'post',
+        //         // body: brandformdata,
+        //         headers: {'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')}
+        //     }).then(response=>response.json())
+        //     .then(data=> {
+
+        //         // console.log(data.success);
+
+        //         // document.getElementById('sucess') = data.success;
+        //         // $('#success').text(data.success).show();
+                
+        //         // $('#updatebrand').modal('hide')
+        //         // $('#updatebrand input').val('')
+        //         // $('#updatebrand input[type="checkbox"]').prop('checked',false)
+        //         // $('#updatebrand select').val('')
+
+        //         document.getElementById('success').innerText = data.success;
+        //         displayallbrands();
+        //     }).catch(error => console.error('Error : ',error));
+
+        // });
 
 
     </script>
