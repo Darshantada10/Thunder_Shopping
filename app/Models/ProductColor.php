2fb15727108dd43c90fbe733c4dfe94f1d79fa2c
Darshantada10 <darshantadaofficial@gmail.com>
2024-08-28 14:43:07 +0530

    new modal and relations

---
+++ app/Models/ProductColor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductColor extends Model
+{
+    use HasFactory;
+    protected $fillable = ['product_id','quantity'];
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function color()
+    {
+        return $this->belongsTo(Color::class);
+    }
+}
