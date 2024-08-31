024beafcaed5707edc716c31f80025ba1597aadb
Darshantada10 <darshantadaofficial@gmail.com>
2024-08-31 14:33:19 +0530

    new methods

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Brand;
+use App\Models\Color;
 use App\Models\Product;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductFormRequest;
 
 class ProductController extends Controller
 {
@@ -20,7 +23,54 @@ class ProductController extends Controller
     {
         $categories = Category::all();
         $brands = Brand::all();
-        return view('Admin.Product.create',compact('categories','brands'));
+        $colors = Color::where('status','0')->get();
+        return view('Admin.Product.create',compact('categories','brands','colors'));
+
+    }
+
+    public function save(ProductFormRequest $request)
+    {
+        // dd("inside controller");
+        $data = $request->validated();
+        // dd($data); 
+        
+        $category = Category::findOrFail($data['category_id']);
+
+        $product = $category->products()->create([
+
+            'category_id' => $data['category_id'],
+            'brand' => $data['brand'],
+            'name' => $data['name'],
+            'slug' => Str::slug($data['slug']),
+            'small_description' => $data['small_description'],
+            'description' => $data['description'],
+            'original_price' => $data['original_price'],
+            'selling_price' => $data['selling_price'],
+            'quantity' => $data['quantity'],
+            'trending' => $request->trending == true ? '1' : '0',
+            'featured' => $request->featured == true ? '1' : '0',
+            'status' => $request->status == true ? '1' : '0',
+            'meta_title' => $data['meta_title'],
+            'meta_keyword' => $data['meta_keyword'],
+            'meta_description' => $data['meta_description'],
+        ]);
+
+        // dd($request);
+        if($request->colorquantity)
+        {
+            foreach($request->colorquantity as $key => $value)
+            {
+                // dd($key,$value);
+                $product->productcolor()->create([
+                    'product_id' => $product->id,
+                    'color_id' => $key,
+                    'quantity' => $value,
+                ]);
+            }
+        }
+
+
+
 
     }
 }
