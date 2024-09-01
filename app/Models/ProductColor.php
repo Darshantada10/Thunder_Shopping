43367e9905c803165309028dde3e72ed1bbd3622
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-01 15:41:13 +0530

    new names

--- app/Models/ProductColor.php
+++ app/Models/ProductColor.php
@@ -8,9 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductColor extends Model
 {
     use HasFactory;
-    protected $fillable = ['product_id','quantity'];
+    protected $fillable = ['product_id','color_id','quantity'];
 
-    public function products()
+    public function product()
     {
         return $this->belongsTo(Product::class);
     }
