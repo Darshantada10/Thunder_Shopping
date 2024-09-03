009f1aabf077ae98d96b13094811a114adf24f1a
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-03 14:59:21 +0530

    name changed

--- database/migrations/2024_08_21_082211_create_products_table.php
+++ database/migrations/2024_08_21_082211_create_products_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
 
             $table->string('name');
             $table->string('slug');
-            $table->string('brand')->nullable();
             $table->mediumText('small_description');
             $table->longText('description');
 
@@ -35,6 +35,7 @@ return new class extends Migration
             $table->longText('meta_description')->nullable();
 
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
 
             $table->timestamps();
         });
