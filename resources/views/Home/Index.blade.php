1cc0c6d089f5d6f363fcf95a49d7482f6d691ae1
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-12 14:44:31 +0530

    displayed trending products

--- resources/views/Home/Index.blade.php
+++ resources/views/Home/Index.blade.php
@@ -13,7 +13,6 @@
         </div>
     @endif
 
-
     <section class="my-lg-14 my-8">
         <div class="container">
             <div class="row">
@@ -26,8 +25,6 @@
 
             <div class="row g-4 row-cols-lg-5 row-cols-2 row-cols-md-3">
 
-
-
                 @foreach ($products as $product)
                     
                 <div class="col">
@@ -36,19 +33,28 @@
 
                             <div class="text-center position-relative ">
                                 <div class=" position-absolute top-0 start-0">
+
+                                    @if ($product->original_price != $product->selling_price)
+                                    
                                     <span class="badge bg-danger">Sale</span>
+                                    @else
+                                        
+                                    <span class="badge bg-success">New</span>
+                                    @endif
+
                                 </div>
-                                <a href="#!"> 
-                                    <img src="{{asset('FrontEnd/Assets/images/products/product-img-1.jpg')}}"
+                                <a href="{{url('/products/'.$product->slug.'/'.$product->id)}}"> 
+                                    {{-- {{dd($product->productimage[0]->image)}} --}}
+                                    <img src="{{$product->productimage[0]->image}}"
                                     {{-- <img src="/FrontEnd/Assets/images/products/product-img-1.jpg" --}}
                                         alt="Grocery Ecommerce Template" class="mb-3 img-fluid"></a>
 
 
                             </div>
-                            <div class="text-small mb-1"><a href="#!"
-                                    class="text-decoration-none text-muted"><small>Brand Name</small></a></div>
-                            <h2 class="fs-6"><a href="pages/shop-single.html"
-                                    class="text-inherit text-decoration-none">{{$product->product_data['name']}}</a></h2>
+                            <div class="text-small mb-1"><a href="{{url('/products/'.$product->slug.'/'.$product->id)}}"
+                                    class="text-decoration-none text-muted"><small>{{$product->category->name}}</small></a></div>
+                            <h2 class="fs-6"><a href="{{url('/products/'.$product->slug.'/'.$product->id)}}"
+                                    class="text-inherit text-decoration-none">{{$product->name}}</a></h2>
                             <div>
 
                                 {{-- <small class="text-warning"> <i class="bi bi-star-fill"></i>
@@ -59,11 +65,20 @@
                                 </small> <span class="text-muted small">4.5(149)</span> --}}
                             </div>
                             <div class="d-flex justify-content-between align-items-center mt-3">
-                                <div><span class="text-dark">${{$product->product_data['discounted_price']}}</span> <span
-                                        class="text-decoration-line-through text-muted">${{$product->product_data['original_price']}}</span>
+                                <div>
+
+                                    @if ($product->selling_price === $product->original_price)
+                                        
+                                    <span class="text-dark">${{$product->selling_price}}</span> 
+                                    @else
+                                    
+                                    <span class="text-dark">${{$product->selling_price}}</span> 
+                                    <span class="text-decoration-line-through text-muted">${{$product->original_price}}</span>
+                                    @endif
+                                
                                 </div>
                                 <div>
-                                    <a href="#!" class="btn btn-primary btn-sm">
+                                    <a href="{{url('/products/'.$product->slug.'/'.$product->id)}}" class="btn btn-primary btn-sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
