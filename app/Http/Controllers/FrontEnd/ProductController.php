e01157e1ab1c0d84bae3fb1d1d0c9f61bad139ca
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-12 14:45:01 +0530

    new methods

---
+++ app/Http/Controllers/FrontEnd/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index($slug,$id)
+    {
+        // $product = Product::findOrFail($id);
+        $product = Product::where('id',$id)->where('slug',$slug)->first();
+        // dd($product);
+        return view('FrontEnd.Product.index',compact('product'));
+    }
+}
